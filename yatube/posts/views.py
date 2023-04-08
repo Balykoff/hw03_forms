1a48b44dfa74e0f3eab836fcfdb7438ae32c29f9
Dmitry balykov <Balykoff@gmail.com>
2023-04-08 15:47:47 +0300

    Fixed and add { csrf_tokens} for update_post.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -77,6 +77,7 @@ def post_edit(request, post_id):
     context = {
         'form': form,
         'is_edit': True,
+        'post_id': post_id
     }
     if request.user != post.author:
         return redirect('posts:post_detail', post_id)
